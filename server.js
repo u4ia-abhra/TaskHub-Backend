41c78f05abf48b139a5763d5382b7a40407a7e1c
u4ia-abhra <abhrajitgh@gmail.com>
2025-11-29 23:54:26 +0530

    Revert "Updated CORS policies"
    This reverts commit ab5142701e0b23af7c9789b19a11838ceabe47cb.

--- server.js
+++ server.js
@@ -26,32 +26,15 @@ const connectToDB = async () => {
   }
 };
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://task-hub-frontend-three.vercel.app",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // set to true only if you plan to use cookies / auth with credentials
-};
-
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",                    
+    "https://task-hub-frontend-three.vercel.app" 
+  ]
+}));
+
 app.use(async (req, res, next) => {
   await connectToDB();
   next();
